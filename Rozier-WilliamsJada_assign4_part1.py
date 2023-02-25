ab9b19dea5d0ab1a01e8df3b0927b3541210895d
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2023-02-25 17:00:13 -0500

    Update Rozier-WilliamsJada_assign4_part1.py

--- Rozier-WilliamsJada_assign4_part1.py
+++ Rozier-WilliamsJada_assign4_part1.py
@@ -1,18 +1,28 @@
 import random
 runner = True
-player1_score = 0
 
 while runner == True:
     player_choice = int(input("How many sides on your dice (4, 6, 8, 10, 12 or 20)? "))
+    if player_choice < 0:
+        print("Invalid size, try again")
+
     die_side = random.randint(1, player_choice)
 
     roll1 = random.randint(1, player_choice)
     roll2 = random.randint(1, player_choice)
     roll3 = random.randint(1, player_choice)
 
-    print(roll1, roll2, roll3)
+    total_roll = roll1 + roll2 + roll3
 
+    print(roll1, roll2, roll3)
+    
     if roll1 % 2 == 1 and roll2 % 2 == 1 and roll3 % 2 == 1:
         print("Odd Roll")
     if roll1 % 2 == 0 and roll2 % 2 == 0 and roll3 % 2 == 0:
         print("Even Roll")
+    if roll1 * roll2 == roll3:
+        print("Multiple Roll")
+    if roll1 + roll2 == roll3:
+        print("Sum Roll")
+    if total_roll == True:
+        print("Total Roll")
