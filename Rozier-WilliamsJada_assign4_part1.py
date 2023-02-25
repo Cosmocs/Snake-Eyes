f4786625e4aa380efc979c168013c0ebe99dbb5d
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2023-02-25 16:19:54 -0500

    Update Rozier-WilliamsJada_assign4_part1.py

--- Rozier-WilliamsJada_assign4_part1.py
+++ Rozier-WilliamsJada_assign4_part1.py
@@ -1,19 +1,18 @@
 import random
-
+runner = True
 player1_score = 0
 
+while runner == True:
+    player_choice = int(input("How many sides on your dice (4, 6, 8, 10, 12 or 20)? "))
+    die_side = random.randint(1, player_choice)
 
-four_side = random.randint(1,4)
-six_side = random.randint(1,6)
-eight_side = random.randint(1,8)
-ten_side = random.randint(1,10)
-twelve_side = random.randint(1,12)
-twenty_side = random.randint(1,20)
+    roll1 = random.randint(1, player_choice)
+    roll2 = random.randint(1, player_choice)
+    roll3 = random.randint(1, player_choice)
 
+    print(roll1, roll2, roll3)
 
-player_choice = int(input("How many sides on your dice (4, 6, 8, 10, 12 or 20)? "))
-
-for i in range(3):
-
-    if(player_choice == "4"):
-        print(four_side)
+    if roll1 % 2 == 1 and roll2 % 2 == 1 and roll3 % 2 == 1:
+        print("Odd Roll")
+    if roll1 % 2 == 0 and roll2 % 2 == 0 and roll3 % 2 == 0:
+        print("Even Roll")
